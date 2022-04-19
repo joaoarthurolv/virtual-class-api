aa1f3a1cd1d9cd8d73b0710c1ad13c46dbbec400
João Arthur <joaoarthur@info.ufrn.br>
2022-04-18 23:17:05 -0300

    Initializing project

---
+++ src/main/java/br/com/virtualclass/VirtualClassApplication.java
@@ -0,0 +1,13 @@
+package br.com.virtualclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/virtualclass/VirtualClassApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.virtualclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
